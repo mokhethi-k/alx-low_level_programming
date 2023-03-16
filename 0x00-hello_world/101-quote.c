9d7848463733935110ad00e80796ca80249a9883
mokhethi-k <mokhethikabelo@gmail.com>
2023-03-16 23:41:02 +0000

    standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include  <unistd.h>
 
 /**
  * main - Entry point
@@ -8,6 +9,6 @@
 
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
