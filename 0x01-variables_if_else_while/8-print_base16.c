79d3701a008841b75609d9673ee4597f393c2daa
mokhethi-k <mokhethikabelo@gmail.com>
2023-03-17 20:17:40 +0000

    printing base 16 numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: printing hex numbers
+ * Return: Always returns 0 on success
+ */
+
+int main(void)
+{
+	int ascii = 48, ascii2 = 97;
+
+	while (ascii <= 57)
+	{
+		putchar(ascii);
+		ascii++;
+	}
+	do {
+		putchar(ascii);
+		ascii++;
+	} while (ascii2 <= 102);
+
+	return (0);
+}
