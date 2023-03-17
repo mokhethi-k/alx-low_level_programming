5c07bf917f15ef613411234e8d9976f8ff2e2f11
mokhethi-k <mokhethikabelo@gmail.com>
2023-03-17 19:34:44 +0000

    printing alphabets except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description 'print alphabets except q and e'
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int ascii = 97;
+
+	while (ascii < 123)
+	{
+		if (ascii == 101 || ascii == 113)
+		{
+
+			continue;
+		}
+
+		putchar(ascii);
+		ascii++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
