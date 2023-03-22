409555c12887b71a348e03fa9b6c1b51fe1c899e
mokhethi-k <mokhethikabelo@gmail.com>
2023-03-22 21:52:45 +0000

    time in 24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,3 +3,23 @@
  * jack_bauer - Prints time in 24hours
  * Return: Returns nothing
  */
+void jack_bauer(void)
+{
+	int i = 0, j;
+
+	while(i < 24)
+	{
+		j= 0;
+		while(j < 60)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j / 10) + '0');
+			_putchar('\n');
+			j++;
+		}
+		i++;
+	}
+}
