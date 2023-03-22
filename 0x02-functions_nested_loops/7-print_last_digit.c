3a7d69f67207100f9196ebad7c96c761baf00061
mokhethi-k <mokhethikabelo@gmail.com>
2023-03-22 01:30:41 +0000

    Returning the last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,8 +6,9 @@
  */
 int print_last_digit(int n)
 {
-	if (n < 0)
-		n *= -1;
-	_putchar(n + '0');
-	return (n);
+	last = n % 10;
+	if (last < 0)
+		last *= -1;
+	_putchar(last + '0');
+	return (last);
 }
