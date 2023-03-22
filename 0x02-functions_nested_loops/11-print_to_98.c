bf3a03b2c3e778b4672255a9577b33c752ab48cd
mokhethi-k <mokhethikabelo@gmail.com>
2023-03-22 20:41:54 +0000

    printing from a given number to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,17 +15,32 @@ void print_to_98(int num)
 		for (i = num; i < 99; i++)
 		{
 			printf("%d", i);
+			if (i != 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 	else if (num == 98)
 	{
 		printf("%d", 98);
+		if (i != 98)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	else if (num > 98)
 	{
-		for (i = num; i > 99; i--)
+		for (i = num; i > 97; i--)
 		{
 			printf("%d", i);
+			if (i != 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 	else if (num < 0)
@@ -33,6 +48,11 @@ void print_to_98(int num)
 		for (i = num; i < 99; i++)
 		{
 			printf("%d", i);
+			if (i != 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 	putchar('\n');
