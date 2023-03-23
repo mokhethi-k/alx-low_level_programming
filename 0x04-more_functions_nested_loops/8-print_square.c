db63da0a5c6d0c5e2047936a408d7000ae32f086
mokhethi-k <mokhethikabelo@gmail.com>
2023-03-23 19:14:01 +0000

    printing a square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,7 +7,6 @@
 
 void print_square(int size)
 {
-	int i, j;
 
 	if (size <= 0)
 	{
@@ -15,13 +14,15 @@ void print_square(int size)
 	}
 	else
 	{
-		for (i = 1; i <= size; i++)
+		int i, j;
+
+		for (i = 0; i < size; i++)
 		{
-			for (j = 1; j <= size; j++)
+			for (j = 0; j < size; j++)
 			{
 				_putchar('#');
 			}
-			_putchar('#');
+			_putchar('\n');
 		}
 	}
 }
