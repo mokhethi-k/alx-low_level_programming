d5ed392ab6b7511b83dd31fa7f2d322a0d63e973
mokhethi-k <mokhethikabelo@gmail.com>
2023-03-23 15:37:15 +0000

    multiplying two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - check for multiples
+ * @a: first number to be checked
+ * @b: Second number to be checked
+ * Return: returns the multiples of the two numbers
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
