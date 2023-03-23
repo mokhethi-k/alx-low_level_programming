7f7cd0e2e00abec250271fef2eeb35fab88c067e
mokhethi-k <mokhethikabelo@gmail.com>
2023-03-23 18:42:47 +0000

    print diagonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * @n: multiplying number
+ *
+ *Return: returns nothing
+ */
+
+void print_diagonal(int n)
+{
+	if (n >= 1)
+	{
+		int i, j;
+
+		for (i = 0; i < i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+				{
+					_putchar('\');
+				}
+				else if (j < i)
+				{
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
