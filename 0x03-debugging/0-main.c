5a53b3111f6ea22ffc953c175a58cd128815de70
mokhethi-k <mokhethikabelo@gmail.com>
2023-03-24 01:35:55 +0000

    Testing the functionality of the file

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - Test function for positive or negative
+ * Return: returns 0 on success
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
