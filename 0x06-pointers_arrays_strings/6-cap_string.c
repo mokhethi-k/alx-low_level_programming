57fcfd3c76440ea519ba61e465a1ff6c436119e4
mokhethi-k <mokhethikabelo@gmail.com>
2023-03-30 12:04:17 +0000

    capitalizing all letters of the string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -18,17 +18,17 @@ char *cap_string(char *str)
 		if (str[index - 1] == ' ' ||
 		        str[index - 1] == '\t' ||
 		        str[index - 1] == '\n' ||
-		    str[index - 1] == ',' ||
-		    str[index - 1] == ';' ||
+		        str[index - 1] == ',' ||
+		        str[index - 1] == ';' ||
 		        str[index - 1] == '.' ||
 		        str[index - 1] == '!' ||
 		        str[index - 1] == '?' ||
 		        str[index - 1] == '"' ||
-		    str[index - 1] == '(' ||
-		    str[index - 1] == ')' ||
-		    str[index - 1] == '{' ||
-		    str[index - 1] == '}' ||
-		    index == 0)
+		        str[index - 1] == '(' ||
+		        str[index - 1] == ')' ||
+		        str[index - 1] == '{' ||
+		        str[index - 1] == '}' ||
+		        index == 0)
 			str[index] -= 32;
 
 		index++;
