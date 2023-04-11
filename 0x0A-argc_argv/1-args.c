c4b2212a7830b18ae17efb677603d752066321e2
mokhethi-k <mokhethikabelo@gmail.com>
2023-04-11 14:41:28 +0000

    Printing the number of arguements

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,9 +9,9 @@
  */
 int main(int argc, char *argv[] __attribute__((unused)))
 {
-	if (argc > 1)
+	if (argc >= 1)
 	{
-		printf("%d", (argc - 1));
+		printf("%d\n", (argc - 1));
 	}
 	return (0);
 }
