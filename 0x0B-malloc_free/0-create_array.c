c2ed5aa9ba2d1c69938d3559de250e6950270529
mokhethi-k <mokhethikabelo@gmail.com>
2023-04-11 20:05:56 +0000

    creating an array using memory allocation

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,17 +13,15 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *s;
 
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
 	s = malloc(sizeof(char) * size);
 	for (i = 0; i < size; i++)
 	{
-		if (size == 0 || s == 0)
-		{
-			return (NULL);
-		}
-		else
-		{
-			s[i] = c;
-		}
+		s[i] = c;
 	}
 	return (s);
 }
