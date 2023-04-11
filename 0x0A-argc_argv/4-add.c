f8dbda8da1ebacb9812c06c33e258aa9afacb02c
mokhethi-k <mokhethikabelo@gmail.com>
2023-04-11 15:52:54 +0000

    Adding positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the sum of positive numbers
+ *@argc: number of arguements passed
+ *@argv: an array of arguements passed
+ *Return: always returns 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (atoi(argv[i]) >= 48 && atoi(argv[i]) <= 57)
+		{
+			sum += atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
