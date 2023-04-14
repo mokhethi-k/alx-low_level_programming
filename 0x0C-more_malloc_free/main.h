f29ea8540e2f6b3c64d216a72dfa16e1d2ab8593
mokhethi-k <mokhethikabelo@gmail.com>
2023-04-14 01:35:46 +0000

    reallocating memory

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -17,4 +17,8 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 int _isdigit(int c);
 
 char *_memset(char *s, char b, unsigned int n);
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+
 #endif
