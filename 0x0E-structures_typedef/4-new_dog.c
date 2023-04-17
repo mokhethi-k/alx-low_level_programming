7a347b1c5c6b51b5af2d84c3dba4d323d1b6201d
mokhethi-k <mokhethikabelo@gmail.com>
2023-04-17 11:08:32 +0000

    creating a new object of struct dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -55,12 +55,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog->name = malloc(sizeof(char) * i + 1);
 	if (new_dog->name == NULL)
 	{
+		free(new_dog);
 		free(new_dog->name);
 		return (NULL);
 	}
 	new_dog->owner = malloc(sizeof(char) * j + 1);
 	if (new_dog->owner == NULL)
 	{
+		free(new_dog);
 		free(new_dog->owner);
 		return (NULL);
 	}
