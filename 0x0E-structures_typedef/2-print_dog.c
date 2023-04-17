7bb3844bb0145a8f7f98f3c4a760f39c3d59ed93
mokhethi-k <mokhethikabelo@gmail.com>
2023-04-17 10:37:54 +0000

    printing the struct information

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,26 +11,16 @@
 void print_dog(struct dog *d)
 {
 
-	d = malloc(sizeof(struct dog));
 	if (d == NULL)
 	{
-		printf("/x2b[1C\n");
 	}
 	if (d->name == NULL)
 	{
 		d->name = "(nil)";
 	}
-	else
-	{
-		printf("name: %s\n", d->name);
-	}
-	printf("age: %f\n", d->age);
 	if (d->owner == NULL)
 	{
 		d->owner = "(nil)";
 	}
-	else
-	{
-		printf("owner: %s", d->owner);
-	}
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
