b67fe3b486c92dc4cfab7f6e13ecdec7295da969
mokhethi-k <mokhethikabelo@gmail.com>
2023-04-21 01:53:27 +0000

    printing integers passed to a function

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ *print_numbers - print numbers
+ *@separator: string between numbers
+ *@n: numbers of integers passed to the function
+ *Return: re turns nothing
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	int ints;
+
+	va_list num;
+
+	va_start(num, n);
+	for (i = 0; i < n; i++)
+	{
+		ints = va_arg(num, int);
+		printf("%d", ints);
+		
+		if (separator != NULL && i < (n - 1))
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(num);
+}
