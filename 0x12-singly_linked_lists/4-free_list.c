0f9cd0a403ec7b297bab439ac2f29e94dd398433
mokhethi-k <mokhethikabelo@gmail.com>
2023-04-28 01:45:02 +0000

    freeing the list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ *free_list - frees the list
+ *@head: pointer to the list
+ *Return: returns nothing
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
