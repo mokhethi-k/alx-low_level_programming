fe0b46a40f05d2b57d1029146b7869d3c319ade2
mokhethi-k <mokhethikabelo@gmail.com>
2023-05-01 19:33:55 +0000

    insrting a node at a given index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -39,6 +39,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		else
 			current = current->next;
 	}
+	if (current  == NULL)
+		return (NULL);
+
 	return (new_n);
 
 }
