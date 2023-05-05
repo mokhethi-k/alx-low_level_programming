652419f96e7e04c6d2da4b901f462bc3b0de1f24
mokhethi-k <mokhethikabelo@gmail.com>
2023-05-05 02:24:23 +0000

    printing the binary representation of a number

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,7 +8,22 @@
 void print_binary(unsigned long int n)
 {
 	unsigned long int num;
+	int flag = 1;
 
-	for (num = 1 << 63; num > 0; num = num >> 2)
-		(n & num) ? _putchar('1') : _putchar('0');
+	num = 1UL << 63;
+	while (num != 0)
+	{
+		if (num & n)
+		{
+			_putchar('1');
+			flag = 0;
+		}
+		else if (flag == 0)
+		{
+			_putchar('0');
+		}
+		num >>= 1;
+	}
+	if (flag == 1 && num == 0)
+		_putchar('0');
 }
