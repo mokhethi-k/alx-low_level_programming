0a5a5c95d258275e470dbe591952e38307f6a4ff
mokhethi-k <mokhethikabelo@gmail.com>
2023-05-10 20:35:28 +0000

    creating a text file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *create_file - creates a file
+ *@filename: pointer to the file to create
+ *@text_content: A text to add to a file
+ *Return: returns 1 on success otherwise -1
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fl, written, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+	fl = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+	while (text_content[len] != '\0')
+		len++;
+	written  = write(fl, text_content, len);
+
+	if (fl == -1 || written == -1)
+		return (-1);
+	close(fl);
+	return (1);
+}
