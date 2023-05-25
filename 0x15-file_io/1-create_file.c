7375b88aceee2bf4f9afc7190becdaa30b0e4f46
mokhethi-k <mokhethikabelo@gmail.com>
2023-05-25 19:59:48 +0000

    correction

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,8 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	while (text_content != NULL)
-		len++;
+		for (len = 0; text_content[len];)
+			len++;
 	fl = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	written  = write(fl, text_content, len);
 
