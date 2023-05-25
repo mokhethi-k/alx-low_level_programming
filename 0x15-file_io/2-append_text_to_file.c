32108c6d1abb970b88b63fcc024881279426460b
mokhethi-k <mokhethikabelo@gmail.com>
2023-05-25 20:20:15 +0000

    correction

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,7 +16,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content;)
+		for (len = 0; text_content[len];)
 			len++;
 	}
 	fl = open(filename, O_WRONLY | O_APPEND);
